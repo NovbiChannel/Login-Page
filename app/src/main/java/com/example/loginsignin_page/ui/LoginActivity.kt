4a9d425dd7e520c71a6aea9abe0a9d4fc2c53688
NovbiChannel <chaglovnikita@gmail.com>
2022-12-20 17:12:29 +0500

    Added the CircleImageView library to display the user's avatar. The function of borrowing an avatar and a username from a google account has been developed.

--- app/src/main/java/com/example/loginsignin_page/ui/LoginActivity.kt
+++ app/src/main/java/com/example/loginsignin_page/ui/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.loginsignin_page.ui
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
@@ -62,34 +63,48 @@ class LoginActivity: AppCompatActivity() {
         binding.btnLogin.setOnClickListener {
 
             lifecycleScope.launch {
-            val userName = binding.etUserName.text.toString()
-            val password = binding.etPassword.text.toString()
+                val userName = binding.etUserName.text.toString()
+                val password = binding.etPassword.text.toString()
 
-                if (viewModel.userLoginChecked(userName, password)) {
-                    val userIdSearch = viewModel.searchIdUser(userName, password)
-                    val userFirstName = viewModel.searchFirstName(userIdSearch)
-                    Snackbar.make(
-                        binding.root,
-                        "We greet you $userName",
-                        Snackbar.LENGTH_LONG
-                    ).show()
+                    if (viewModel.userLoginChecked(userName, password)) {
+                        val userIdSearch = viewModel.searchIdUser(userName, password)
+                        val userFirstName = viewModel.searchFirstName(userIdSearch)
+                        Snackbar.make(
+                            binding.root,
+                            "We greet you $userName",
+                            Snackbar.LENGTH_LONG
+                        ).show()
 
-                    val i = Intent(this@LoginActivity, MainActivity::class.java)
-                    i.putExtra(USER_FIRST_NAME, userFirstName)
-                    startActivity(i)
+                        val i = Intent(this@LoginActivity, MainActivity::class.java)
+                        i.putExtra(USER_FIRST_NAME, userFirstName)
+                        startActivity(i)
 
-                } else {
-                    Snackbar.make(
-                        binding.root,
-                        "Invalid username or password, please check the entered data",
-                        Snackbar.LENGTH_LONG
-                    ).show()
-                }
+                    } else {
+                        Snackbar.make(
+                            binding.root,
+                            "Invalid username or password, please check the entered data",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
             }
         }
 
         binding.tvDontHaveAnAccount.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))
+        }
+
+        clearFields()
+    }
+
+    private fun clearFields() {
+        binding.etUserName.text.clear()
+        binding.etPassword.text.clear()
+    }
+
+    private fun checkAuthState() {
+        if (auth.currentUser != null) {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
         }
     }
 
@@ -117,6 +132,7 @@ class LoginActivity: AppCompatActivity() {
                     "You have successfully logged in",
                     Snackbar.LENGTH_LONG
                 ).show()
+                checkAuthState()
             } else {
                 Snackbar.make(
                     binding.root,
--- app/src/main/java/com/example/loginsignin_page/ui/MainActivity.kt
+++ app/src/main/java/com/example/loginsignin_page/ui/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.loginsignin_page.ui
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
+import com.example.loginsignin_page.R
 import com.example.loginsignin_page.databinding.ActivityMenuBinding
 import com.example.loginsignin_page.other.Constants.USER_FIRST_NAME
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 import java.time.LocalTime
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMenuBinding
+    lateinit var auth: FirebaseAuth
+    private lateinit var userName: String
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val time = LocalTime.now().hour
 
     @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("SetTextI18n")
@@ -20,30 +30,52 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+        userName = ""
+
         val userFirstName = intent.getStringExtra(USER_FIRST_NAME)
-        val time = LocalTime.now()
+        val userNameGoogle = auth.currentUser?.displayName.toString()
+
+        val userImgGoogle = auth.currentUser?.photoUrl
+
+        if (userFirstName == null) {
+            userName = userNameGoogle
+            Picasso.get().load(userImgGoogle).into(binding.ivUserPhoto)
+        } else {
+            userName = userFirstName
+        }
+
+        binding.ivCalendar.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        greetingsText()
+    }
+
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun greetingsText() {
 
         for (i in 0..5) {
-            if (i == time.hour) {
-                binding.tvHelloUser.text = "Goodnight, $userFirstName"
+            if (i == time) {
+                binding.tvHelloUser.text = "Goodnight, $userName"
             }
         }
 
         for (i in 6..10) {
-            if (i == time.hour) {
-                binding.tvHelloUser.text = "Good morning, $userFirstName"
+            if (i == time) {
+                binding.tvHelloUser.text = "Good morning, $userName"
             }
         }
 
         for (i in 11..17) {
-            if (i == time.hour) {
-                binding.tvHelloUser.text = "Good afternoon, $userFirstName"
+            if (i == time) {
+                binding.tvHelloUser.text = "Good afternoon, $userName"
             }
         }
 
         for (i in 18..23) {
-            if (i == time.hour) {
-                binding.tvHelloUser.text = "Good evening, $userFirstName"
+            if (i == time) {
+                binding.tvHelloUser.text = "Good evening, $userName"
             }
         }
     }
